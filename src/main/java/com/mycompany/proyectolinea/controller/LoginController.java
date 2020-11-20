1ca88bf2c60ba3e331a2df31b26c58a5f084b69a
FernandoArevalo21 <fernandoarevalo2000@hotmail.com>
2020-11-19 23:39:44 -0500

    Login ya esta en funcionamiento

--- src/main/java/com/mycompany/proyectolinea/controller/LoginController.java
+++ src/main/java/com/mycompany/proyectolinea/controller/LoginController.java
@@ -6,8 +6,13 @@
 package com.mycompany.proyectolinea.controller;
 
 import com.mycompany.proyectolinea.servic.Loginservice;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -23,9 +28,20 @@ String password;
      */
     public LoginController() {
     }
-    public void Login(){
+    public void Login() throws IOException{
+    try {
         System.out.println("Entro " + correo + " " + password);
         Loginservice service = new Loginservice();
+        if(service.VerDatosComprador(correo,password).equals("Admin")){
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/ProyectoLinea/faces/Administrador.xhtml");
+        }else if(service.VerDatosComprador(correo,password).equals("comprador")){
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/ProyectoLinea/faces/index.xhtml");
+        }else{
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/ProyectoLinea/faces/Error.xhtml"); 
+        }
+    } catch (Exception ex) {
+        Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+    }
     }
 
     public String getCorreo() {
--- src/main/java/com/mycompany/proyectolinea/servic/Loginservice.java
+++ src/main/java/com/mycompany/proyectolinea/servic/Loginservice.java
@@ -5,11 +5,14 @@
  */
 package com.mycompany.proyectolinea.servic;
 
+import com.mycompany.proyectolinea.conexion.conexion;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
@@ -19,23 +22,42 @@ import javax.inject.Named;
  * @author usuario
  */
 public class Loginservice {
+
     //mirar como hacer pruebas porque no sea podido ejecutar
-     Connection conect = null;
-       Statement st;
-    public void VerDatosComprador() throws SQLException, ClassNotFoundException{
-         Class.forName("com.mysql.jdbc.Driver");
-         conect = DriverManager.getConnection("jdbc:mysql://localhost/proyectolinea","root","");
-         st = conect.createStatement();
-         ResultSet rs = st.executeQuery("SELECT*FROM comprador");
-         while(rs.next()){
-             int id_comprador = rs.getInt("id_comprador");
-             String nombre = rs.getString("nombre");
-             String apellido = rs.getString("apellido");
-             String correo = rs.getString ("correo");
-             String username = rs.getString("username");
-             String password = rs.getString("password");
-             System.out.println(String.format("%s,%s,%s,%s,%s,%s",id_comprador,nombre,apellido,correo,username,password));
-         }
-         
+    Connection conect = null;
+    Statement st;
+    String cor;
+    String pass;
+    String rol;
+     conexion conexion = new conexion();
+ 
+    public String VerDatosComprador(String correo, String password) throws SQLException {
+                    st = conexion.conexion().createStatement();
+        try {
+            ResultSet rs = st.executeQuery("SELECT*FROM comprador where correo='" + correo + "' and password='" + password + "'");
+            while (rs.next()) {
+                cor = rs.getString("correo");
+                pass = rs.getString("password");
+                System.out.println(String.format("%s,%s", cor, pass));
+            }
+            if(cor.equalsIgnoreCase(correo)&& pass.equalsIgnoreCase(password)){
+                rol="comprador";
+            }
+        } catch (Exception ex) {
+            try {
+                ResultSet rs = st.executeQuery("SELECT*FROM admin where correo='" + correo + "' and password='" + password + "'");
+                while (rs.next()) {
+                    cor = rs.getString("correo");
+                    pass = rs.getString("password");
+                    System.out.println(String.format("%s,%s", cor, pass));
+                }
+                if(cor.equalsIgnoreCase(correo)&& pass.equalsIgnoreCase(password)){
+                rol="Admin";
+            }
+            } catch (Exception ex1) {
+                rol=null;
+            }
+        }
+        return rol;
     }
 }
